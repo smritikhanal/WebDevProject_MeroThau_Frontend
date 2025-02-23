11f2bc6d8d0add20d77eabc98e66752b86b95c87
Smriti Khanal <smritikhanal45@gmail.com>
2025-02-23 16:03:48 +0545

    Main server

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import necessary components from react-router-dom
+import Home from "./Pages/HomePage";
+import UploadProfile from "./Pages/UploadProfile"; // Import Home component
+import AboutUs from "./Pages/AboutUs"; // Example: AboutUs component
+import Services from "./Pages/Services"; // Example: Services component
+import Allhotels from "./Pages/Allhotels"; 
+import ContactUs from "./Pages/ContactUs"; // Example: ContactUs component
+import Login from "./Pages/Login"; // Example: Login component
+import ForgotPassword from "./Pages/ForgotPassword";
+import Register from "./Pages/Register";
+import Fishtail from "./Hotels/Fishtail"; 
+import HotelMalla from "./Hotels/HotelMalla";
+import KarmaVilla from './Hotels/KarmaVilla';
+import LakesideStay from './Hotels/LakesideStay';
+import Confirmation from './Pages/Confirmation';
+import ReviewFeedback from './Pages/ReviewFeedback';
+import ManageHotel from './Admin/ManageHotel';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> 
+        <Route path="/profile" element={<UploadProfile />} />
+        <Route path="/home" element={<Home />} /> 
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/aboutus" element={<AboutUs />} /> 
+        <Route path="/services" element={<Services />} /> 
+        <Route path="/all-hotels" element={<Allhotels />} />
+        <Route path="/Hotels/Fishtail" element={<Fishtail />} /> 
+         <Route path="/Hotels/HotelMalla" element={<HotelMalla />} /> 
+         <Route path="/Hotels/KarmaVilla" element={<KarmaVilla />} /> 
+         <Route path="/Hotels/LakesideStay" element={<LakesideStay />} /> 
+         <Route path="/Pages/Confirmation" element={<Confirmation />} /> 
+        <Route path="/contactus" element={<ContactUs />} /> 
+        <Route path="/ReviewFeedback" element={<ReviewFeedback />} />
+        <Route path="/ManageHotel" element={<ManageHotel />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
